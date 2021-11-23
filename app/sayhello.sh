2360c29dbdf369ca20aa712d3717605324f9f446
Manoj Baghela <you@example.com>
2021-11-24 03:06:37 +0530

    changes

--- app/sayhello.sh
+++ app/sayhello.sh
@@ -15,5 +15,5 @@ until curl --output /dev/null --silent --head --fail "$WAIT_HOSTS"; do
   >&2 echo "Mysql is unavailable - sleeping"
   sleep 1
 done
->&2 
+>&2
 source /usr/local/bin/docker-entrypoint
